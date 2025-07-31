b3c6a92c3d06086815e7f70fb0372ee6f2c362d0
brian <contact@noimbrian.com>
2025-07-31 18:06:59 -0500

    chore: begin styling

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="layout-app h-full w-full bg-base-darker">
+    <header class="h-full w-full py-8 surface-base-dark"></header>
+	<aside class="h-full w-full surface-base-lighter"></aside>
+	<main>
+		{@render children()}
+	</main>
+</div>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -5,6 +5,18 @@
 	const { supabase, locations } = data;
 </script>
 
+<div class="flex gap-4 text-center *:flex *:items-center *:justify-center p-16 flex-wrap">
+	<div class="h-24 w-24 surface-base-lighter">Lighter</div>
+	<div class="h-24 w-24 surface-base-light">Light</div>
+	<div class="h-24 w-24 surface-base">Surface</div>
+	<div class="h-24 w-24 surface-base-dark">Dark</div>
+	<div class="h-24 w-24 surface-base-darker">Darker</div>
+	<div class="h-24 w-24 surface-success">Success</div>
+	<div class="h-24 w-24 surface-warning">Warning</div>
+	<div class="h-24 w-24 surface-danger">Danger</div>
+	<div class="h-24 w-24 surface-info">Info</div>
+</div>
+
 {#each locations as location}
 	<div>{location.name}</div>
 {/each}
